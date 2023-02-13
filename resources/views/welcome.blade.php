4b95a22568cffaa9af938df7d64506408c738bef
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-02-13 10:41:16 +0330

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,6 @@
     </head>
     <body class="antialiased">
         <div id="app">
-
         </div>
     </body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->get('/user',function(Request $request){
+return $request->user();
 });
+
+Route::get('/items',[ItemController::class,'index']);
